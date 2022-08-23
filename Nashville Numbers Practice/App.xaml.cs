96a63c814c3fc5f769963f6a94da55f34f117374
Sam Fahnestock <samiam21@gmail.com>
2022-08-23 17:04:32 -0400

    Template app

---
+++ Nashville Numbers Practice/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Nashville_Numbers_Practice;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Nashville Numbers Practice/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Nashville_Numbers_Practice;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
