56643a47388b7094036cda22c8190d552661ef2c
Sam Fahnestock <samiam21@gmail.com>
2022-08-24 23:14:15 -0400

    Finish displaying numbers for learning

--- NashvilleNumberPractice/Data/NashvilleNumberData.cs
+++ NashvilleNumberPractice/Data/NashvilleNumberData.cs
@@ -4,10 +4,13 @@
     {
         public static Dictionary<string, string[]> NashvilleNumbers = new Dictionary<string, string[]>() 
         {
-            {"G", new string[6]{"G", "Am", "Bm", "C", "D", "Em"} },
-            {"A", new string[6]{"G", "Am", "Bm", "C", "D", "Em"} },
-            {"D", new string[6]{"G", "Am", "Bm", "C", "D", "Em"} },
-            {"C", new string[6]{"G", "Am", "Bm", "C", "D", "Em"} }
+            {"A", new string[6]{"A", "Bm", "Cm", "D", "E", "F#m"} },
+            {"B", new string[6]{"B", "C#m", "D#m", "E", "F#", "G#m"} },
+            {"C", new string[6]{"C", "Dm", "Em", "F", "G", "Am"} },
+            {"D", new string[6]{"D", "Em", "F#m", "G", "A", "Bm"} },
+            {"E", new string[6]{"E", "F#m", "G#m", "A", "B", "C#m"} },
+            {"F", new string[6]{"F", "Gm", "Am", "Bb", "C", "Dm"} },
+            {"G", new string[6]{"G", "Am", "Bm", "C", "D", "Em"} }
         };
     }
 }
