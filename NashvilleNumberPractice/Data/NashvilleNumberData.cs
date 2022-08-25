0cfebd7c0f87fa5e0f9a0b6511381ce5c8477594
Sam Fahnestock <samiam21@gmail.com>
2022-08-24 20:19:22 -0400

    Begin the learn page

--- NashvilleNumberPractice/Data/NashvilleNumberData.cs
+++ NashvilleNumberPractice/Data/NashvilleNumberData.cs
@@ -2,5 +2,12 @@
 {
     public static class NashvilleNumberData
     {
+        public static Dictionary<string, string[]> NashvilleNumbers = new Dictionary<string, string[]>() 
+        {
+            {"G", new string[6]{"G", "Am", "Bm", "C", "D", "Em"} },
+            {"A", new string[6]{"G", "Am", "Bm", "C", "D", "Em"} },
+            {"D", new string[6]{"G", "Am", "Bm", "C", "D", "Em"} },
+            {"C", new string[6]{"G", "Am", "Bm", "C", "D", "Em"} }
+        };
     }
 }
