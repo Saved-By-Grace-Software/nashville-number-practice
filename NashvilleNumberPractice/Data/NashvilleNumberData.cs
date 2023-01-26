13fe9b2647e8c1fec03baed9264c3e6eb89005a1
Sam Fahnestock <samiam21@gmail.com>
2023-01-25 21:27:50 -0500

    Add 7th for each key

--- NashvilleNumberPractice/Data/NashvilleNumberData.cs
+++ NashvilleNumberPractice/Data/NashvilleNumberData.cs
@@ -2,15 +2,17 @@
 {
     public static class NashvilleNumberData
     {
+        public static readonly int NUM_CHORDS_IN_KEY = 7;
+
         public static Dictionary<string, string[]> NashvilleNumbers = new Dictionary<string, string[]>() 
         {
-            {"A", new string[6]{"A", "Bm", "Cm", "D", "E", "F#m"} },
-            {"B", new string[6]{"B", "C#m", "D#m", "E", "F#", "G#m"} },
-            {"C", new string[6]{"C", "Dm", "Em", "F", "G", "Am"} },
-            {"D", new string[6]{"D", "Em", "F#m", "G", "A", "Bm"} },
-            {"E", new string[6]{"E", "F#m", "G#m", "A", "B", "C#m"} },
-            {"F", new string[6]{"F", "Gm", "Am", "Bb", "C", "Dm"} },
-            {"G", new string[6]{"G", "Am", "Bm", "C", "D", "Em"} }
+            {"A", new string[7]{"A", "Bm", "Cm", "D", "E", "F#m", "G#"} },
+            {"B", new string[7]{"B", "C#m", "D#m", "E", "F#", "G#m", "A#"} },
+            {"C", new string[7]{"C", "Dm", "Em", "F", "G", "Am", "B"} },
+            {"D", new string[7]{"D", "Em", "F#m", "G", "A", "Bm", "C#"} },
+            {"E", new string[7]{"E", "F#m", "G#m", "A", "B", "C#m", "D#"} },
+            {"F", new string[7]{"F", "Gm", "Am", "Bb", "C", "Dm", "E"} },
+            {"G", new string[7]{"G", "Am", "Bm", "C", "D", "Em", "F#"} }
         };
     }
 }
